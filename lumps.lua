4d985d8536a91de9deac434b375a8e7bf6630142
AntumDeluge <antumdeluge@gmail.com>
2017-06-23 01:28:56 -0700

    Register lumps:
    - minerals:copper_lump
    - minerals:gold_lump
    - minerals:iron_lump
    - minerals:tin_lump

--- lumps.lua
+++ lumps.lua
@@ -8,6 +8,7 @@
 --]]
 
 
+-- Coal
 minetest.register_craftitem('minerals:coal_lump', {
 	description = 'Coal Lump',
 	inventory_image = 'minerals_coal_lump.png',
@@ -15,4 +16,28 @@ minetest.register_craftitem('minerals:coal_lump', {
 		coal=1,
 		flammable=1,
 	},
+})
+
+-- Copper
+minetest.register_craftitem('minerals:copper_lump', {
+	description = 'Copper Lump',
+	inventory_image = 'minerals_copper_lump.png',
+})
+
+-- Gold
+minetest.register_craftitem('minerals:gold_lump', {
+	description = 'Gold Lump',
+	inventory_image = 'minerals_gold_lump.png',
+})
+
+-- Iron
+minetest.register_craftitem('minerals:iron_lump', {
+	description = 'Iron Lump',
+	inventory_image = 'minerals_iron_lump.png',
+})
+
+-- Tin
+minetest.register_craftitem('minerals:tin_lump', {
+	description = 'Tin Lump',
+	inventory_image = 'minerals_tin_lump.png',
 })
