3bc9c8ddaef67d2ee3cfaab63a8ce86e5502edf7
AntumDeluge <antumdeluge@gmail.com>
2017-06-23 01:12:52 -0700

    Register 'minerals:coal_lump' craft item

--- lumps.lua
+++ lumps.lua
@@ -8,3 +8,11 @@
 --]]
 
 
+minetest.register_craftitem('minerals:coal_lump', {
+	description = 'Coal Lump',
+	inventory_image = 'minerals_coal_lump.png',
+	groups = {
+		coal=1,
+		flammable=1,
+	},
+})
