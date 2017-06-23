0ae3d4ce648f058dcf31809387a08f49b82fecf4
AntumDeluge <antumdeluge@gmail.com>
2017-06-23 01:12:27 -0700

    Register 'minerals:coal' node

--- nodes.lua
+++ nodes.lua
@@ -8,3 +8,28 @@
 --]]
 
 
+--[[ ORES
+
+default:
+  - minerals:coal
+  - minerals:copper
+  - minerals:diamond
+  - minerals:gold
+  - minerals:iron
+  - minerals:mese
+  - minerals:tin
+
+]]
+
+
+-- Coal
+minetest.register_node('minerals:coal', {
+	description = 'Coal Ore',
+	tiles = {'default_stone.png^minerals_coal.png'},
+	groups = {cracky = 3},
+	drop = 'minerals:coal_lump',
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+-- Iron
