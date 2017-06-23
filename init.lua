3b69de52608d796fc08a86afe924937cca8a71db
AntumDeluge <antumdeluge@gmail.com>
2017-06-23 16:37:35 -0700

    Add global setting 'minerals.disabled_minerals':
    List of minerals to disable.

--- init.lua
+++ init.lua
@@ -12,6 +12,21 @@ minerals = {}
 minerals.modname = minetest.get_current_modname()
 minerals.modpath = minetest.get_modpath(minerals.modname)
 
+-- List of minerals to disable
+minerals.disabled_minerals = {}
+-- Split comma-separated (& semi-color-sparated) list into table
+for index, mineral in ipairs(string.split(minetest.settings:get('minerals.disabled_minerals'), ',')) do
+	if string.find(mineral, ';') then
+		-- Semi-colons
+		for index, mineral in ipairs(string.split(mineral, ';')) do
+			table.insert(minerals.disabled_minerals, mineral)
+		end
+	else
+		-- No semi-colon
+		table.insert(minerals.disabled_minerals, mineral)
+	end
+end
+
 
 local scripts = {
 	'functions',
@@ -23,4 +38,13 @@ local scripts = {
 
 for index, script in ipairs(scripts) do
 	dofile(minerals.modpath .. '/' .. script .. '.lua')
+end
+
+
+-- DEBUG:
+if minerals.disabled_minerals and minetest.settings:get_bool('log_mods') then
+	minerals.log('Disabled minerals:')
+	for index, mineral in ipairs(minerals.disabled_minerals) do
+		minerals.log('  ' .. mineral)
+	end
 end
